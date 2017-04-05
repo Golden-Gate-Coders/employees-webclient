9a7e28d4621fd7097f93de9af82e0e8bf0f16acb
Danish Mughal <mughal.danish@gmail.com>
2017-04-04 20:06:43 -0700

    Added new,create

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -4,4 +4,26 @@ class EmployeesController < ApplicationController
 
     @employee = Unirest.get("http://localhost:3000/api/v1/employees/#{@id}.json").body
   end
+
+  def index
+    @employees = Unirest.get("http://localhost:3000/api/v1/employees.json").body
+  end
+
+  def new
+  end
+
+  def create
+    @employee = Unirest.post("http://localhost:3000/api/v1/employees",
+        :headers => { "Accept" => "application/json" },
+        :parameters => { :name => params[:name],
+            :email => params[:email],
+            :birthdate => params[:birthdate],
+            :ssn => params[:ssn]
+        }
+      ).body
+
+    redirect_to "/employees/#{@employee['id']}"
+  end
+
+
 end
