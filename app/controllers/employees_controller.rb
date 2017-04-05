698a66b0f7f685fe69cfdbc17f5371ed72f83b3a
Danish Mughal <mughal.danish@gmail.com>
2017-04-04 21:25:09 -0700

    Added edit, update, and destroy

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -26,4 +26,32 @@ class EmployeesController < ApplicationController
   end
 
 
+  def edit
+    @employee_id = params[:id]
+    @employee = Unirest.get("http://localhost:3000/api/v1/employees/#{@employee_id}.json").body
+  end
+
+  def update
+    @employee = Unirest.patch("http://localhost:3000/api/v1/employees/#{params[:id]}",
+        :headers => { "Accept" => "application/json" },
+        :parameters => { :name => params[:name],
+            :email => params[:email],
+            :birthdate => params[:birthdate],
+            :ssn => params[:ssn]
+        }
+      ).body
+
+    redirect_to "/employees/#{@employee['id']}"
+  end
+
+  def destroy
+    Unirest.delete("http://localhost:3000/api/v1/employees/#{params[:id]}", 
+      :headers => { "Accept" => "application/json" })
+
+    redirect_to "/employees"
+  end
+
+
+
+
 end
